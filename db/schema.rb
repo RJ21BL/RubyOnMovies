cf0fb6274edd7171e7e52a9f184b1f45a42a765b
Richard Lawrence <richard.lawrence@simplybusiness.co.uk>
2022-06-16 10:39:42 +0100

    list seeded data and adjust genre column name as previous column was reserved in rails

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_15_162505) do
+ActiveRecord::Schema.define(version: 2022_06_16_091302) do
 
   create_table "directors", force: :cascade do |t|
     t.string "name"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2022_06_15_162505) do
   end
 
   create_table "genres", force: :cascade do |t|
-    t.string "type"
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ Movie.destroy_all
 Genre.destroy_all
 Director.destroy_all
 
-Movie.create(
+Movie.create([
   {title: 'The Godfather', year: '1972'},
   {title: 'The Godfather: Part II', year: '1974'},
   {title: 'The Godfather: Part III', year: '1990'},
@@ -23,23 +23,23 @@ Movie.create(
   {title: 'Inside Man', year: '2006'},
   {title: 'Jurrassic Park', year: '1993'},
   {title: 'Paranormal Activity', year: '2007'}
-)
+])
 
-Genre.create(
-  {type: 'Crime/Drama'},
-  {type: 'Action/Comedy'},
-  {type: 'Crime/Thriller'},
-  {type: 'Science Fiction'},
-  {type: 'Horror'}
-)
+Genre.create([
+  {name: 'Crime/Drama'},
+  {name: 'Action/Comedy'},
+  {name: 'Crime/Thriller'},
+  {name: 'Science Fiction'},
+  {name: 'Horror'}
+])
 
-Director.create(
+Director.create([
   {name: 'Francis Coppolla'},
   {name: 'Brett Ratner'},
   {name: 'Antonie Fuqua'},
   {name: 'Spike Lee'},
   {name: 'Steven Spielberg'},
   {name: 'Oren Peli'}
-)
+])
 
 puts 'Seeding complete.'
