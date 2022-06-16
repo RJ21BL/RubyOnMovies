6cbad89eaf06fb02d97e61963b3d306577a54aee
Richard Lawrence <richard.lawrence@simplybusiness.co.uk>
2022-06-16 14:56:36 +0100

    add foreign keys for movie and genre table

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,3 @@
 class Movie < ApplicationRecord
   belongs_to :directors
-  has_many :genres
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_16_095433) do
+ActiveRecord::Schema.define(version: 2022_06_16_134902) do
 
   create_table "directors", force: :cascade do |t|
     t.string "name"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 2022_06_16_095433) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "movie_id"
+    t.index ["movie_id"], name: "index_genres_on_movie_id"
   end
 
   create_table "movies", force: :cascade do |t|
@@ -36,5 +38,10 @@ ActiveRecord::Schema.define(version: 2022_06_16_095433) do
     t.integer "year"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "genre_id"
+    t.index ["genre_id"], name: "index_movies_on_genre_id"
   end
+
+  add_foreign_key "genres", "movies"
+  add_foreign_key "movies", "genres"
 end
