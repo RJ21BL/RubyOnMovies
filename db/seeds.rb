53a3fc5c18f00c96f20f01f4bd88b5db3ff2c4c0
Richard Lawrence <richard.lawrence@simplybusiness.co.uk>
2022-06-16 09:50:22 +0100

    seed the database with movies, genres and directors

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Seeding..'
+
+Movie.destroy_all
+Genre.destroy_all
+Director.destroy_all
+
+Movie.create(
+  {title: 'The Godfather', year: '1972'},
+  {title: 'The Godfather: Part II', year: '1974'},
+  {title: 'The Godfather: Part III', year: '1990'},
+  {title: 'Rush Hour', year: '1998'},
+  {title: 'Rush Hour 2', year: '2001'},
+  {title: 'Rush Hour 3', year: '2007'},
+  {title: 'Training Day', year: '2001'},
+  {title: 'Inside Man', year: '2006'},
+  {title: 'Jurrassic Park', year: '1993'},
+  {title: 'Paranormal Activity', year: '2007'}
+)
+
+Genre.create(
+  {type: 'Crime/Drama'},
+  {type: 'Action/Comedy'},
+  {type: 'Crime/Thriller'},
+  {type: 'Science Fiction'},
+  {type: 'Horror'}
+)
+
+Director.create(
+  {name: 'Francis Coppolla'},
+  {name: 'Brett Ratner'},
+  {name: 'Antonie Fuqua'},
+  {name: 'Spike Lee'},
+  {name: 'Steven Spielberg'},
+  {name: 'Oren Peli'}
+)
+
+puts 'Seeding complete.'
