c562606547e8b8027fa0d67543e0f4108f42dc55
Richard Lawrence <richard.lawrence@simplybusiness.co.uk>
2022-06-20 22:48:20 +0100

    add rspec to rails

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2022_06_16_214629) do
-
   create_table "directors", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
