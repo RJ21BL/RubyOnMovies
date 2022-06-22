e0946424d074e5ac5246c5d4ca1227c1b8fd549c
Richard Lawrence <richard.lawrence@simplybusiness.co.uk>
2022-06-22 15:54:44 +0100

    added test cases for index and new

--- spec/requests/movies_request_spec.rb
+++ spec/requests/movies_request_spec.rb
@@ -1,10 +1,18 @@
-# require './app/controllers/movies_controller'
 require 'rails_helper'
-require 'spec_helper'
 
 describe 'Movies', type: :request do
-  it 'sends a get request to the root route' do
+  it 'sends a get request to get the root URL' do
     get '/'
+    expect(response).to be_successful
+  end
+  
+  it 'sends a get request to get the movies URL' do
+    get '/movies'
+    expect(response).to be_successful
+  end
+
+  it 'sends a get request to get the new URL ' do
+    get '/movies/new'
     expect(response).to be_successful
   end
 
@@ -13,7 +21,7 @@ describe 'Movies', type: :request do
     assert_response :success
 
     post '/movies', params: { 
-      movie: {title: 'James Bond', year: 2002} 
+      movie: { title: 'James Bond', year: 2002 }
     }
     assert_response :redirect
     follow_redirect!
