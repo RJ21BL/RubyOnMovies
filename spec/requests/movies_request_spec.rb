d19ded17d75d18da8c0d8da69099ee55856a9004
Richard Lawrence <richard.lawrence@simplybusiness.co.uk>
2022-06-22 18:05:29 +0100

    improve readability

--- spec/requests/movies_request_spec.rb
+++ spec/requests/movies_request_spec.rb
@@ -3,28 +3,26 @@ require 'rails_helper'
 describe 'Movies', type: :request do
   it 'sends a get request to get the root URL' do
     get '/'
+
     expect(response).to be_successful
   end
   
   it 'sends a get request to get the movies URL' do
     get '/movies'
+
     expect(response).to be_successful
   end
 
-  it 'sends a get request to get the new URL ' do
+  it 'creates a movie and redirects to the new URL' do
     get '/movies/new'
+
     expect(response).to be_successful
-  end
-
-  it 'creates a new movie' do
-    get '/movies/new'
-    assert_response :success
-
-    post '/movies', params: { 
-      movie: { title: 'James Bond', year: 2002 }
-    }
+    
+    post '/movies', 
+      params: { movie: { title: 'Die Another Day', year: 2002 } }
     assert_response :redirect
     follow_redirect!
-    assert_response :success
+
+    expect(response).to be_successful
   end
 end
