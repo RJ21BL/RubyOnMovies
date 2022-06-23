0db63189b5adfeb5a0bb5d9a12702a6fb34932ae
Richard Lawrence <richard.lawrence@simplybusiness.co.uk>
2022-06-23 16:01:01 +0100

    ensure the show path is rendered when creating a movie object

--- spec/requests/movies_request_spec.rb
+++ spec/requests/movies_request_spec.rb
@@ -24,9 +24,19 @@ describe 'Movies', type: :request do
   end
 
   it 'renders the edit path' do
-    movie = Movie.create({ title: 'The Godfather', year: '1972' })
+    movie = Movie.create(title: 'The Godfather', year: '1972')
     get "/movies/#{movie.id}/edit"
 
     expect(response).to render_template('movies/edit')
+  end
+
+  it 'renders the show path' do
+    director = Director.create(id: 1, name: 'Peter Jackson')
+    genre = Genre.create(id: 1, name: 'Adventure')
+    movie = Movie.create(id: 1, title: 'The Felowship Of The Ring', year: 2001, director_id: 1, genre_id: 1)
+
+    get "/movies/#{movie.id}"
+
+    expect(response).to render_template('movies/show')
   end
 end
