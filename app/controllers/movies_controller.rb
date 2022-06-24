0ebad0116abaa1c68fb89c65130561e4a7494e4f
Richard Lawrence <richard.lawrence@simplybusiness.co.uk>
2022-06-24 16:06:15 +0100

    adjust code to accept nested attributes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -40,6 +40,6 @@ class MoviesController < ApplicationController
   private
 
   def movie_params
-    params.require(:movie).permit(:title, :year)
+    params.require(:movie).permit(:title, :year, directors_attributes: [:name])
   end
 end
--- app/models/director.rb
+++ app/models/director.rb
@@ -1,5 +1,5 @@
 class Director < ApplicationRecord
   has_many :movies
   has_and_belongs_to_many :genres, optional: true
-  accepts_nested_attributes_for :movies, :genres
+  accepts_nested_attributes_for :movies
 end
