3ce8e4b0ce224b2fcf95f8b16952fc390b916f82
Richard Lawrence <richard.lawrence@simplybusiness.co.uk>
2022-07-06 11:56:13 +0100

    create feature tests to ensure search bar searches by title and raises an error if the title does not exist

---
+++ spec/features/search_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe 'search bar', type: :feature do
+  it 'searches for a movie by title' do
+    visit movies_path
+    movie = FactoryBot.create(:movie)
+    fill_in(:search_bar, with: movie.title)
+    click_button('Search movie')
+    
+    expect(page).to have_content(movie.title)
+  end
+
+  it 'shows an error message if the movie does not exist' do
+    visit movies_path
+    movie = 'Alien'
+    error_msg = 'Movie not found'
+    fill_in(:search_bar, with: movie)
+    click_button('Search movie')
+
+    expect(page).to have_content(error_msg)
+  end
+end
